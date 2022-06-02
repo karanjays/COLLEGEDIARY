0491bf8dc6c0b4c7b230f5347067abf8f8861427
Karan <karanjayswal353@hotmail.com>
2022-06-02 10:57:17 +0530

    connection of database is done

--- controllers/postcontroller.js
+++ controllers/postcontroller.js
@@ -1,3 +1,46 @@
-exports.createNewPost = (req, res) => {
-    res.render("post");
+const Post=require("../model/post");
+
+exports.createNewPost = async (req,res)=>
+{
+    let postData = new Post({
+        title:req.body.title,
+        content:req.body.content,
+        type:"notices",
+        author:"Karan",
+        date: new Date(),
+        upvote: 3
+    });
+
+    // callback
+    // postData.save((err,post)=>
+    // {
+    // if(err)
+    //     console.log(err);
+    // else
+    //     console.log(post);
+    // }
+    // );
+
+    //promices
+    // postData.save()
+    // .then(post=>{
+    //     console.log(post);
+    // })
+    // .catch(err=>{
+    //     console.log(err);
+    // });
+
+
+    // async and await
+    try
+    {
+    await postData.save();
+    // console.log(postData);
+    const blogs = await Post.find({});
+    // console.log(blogs);
+    res.render("home");
+    }catch(err)
+    {
+        console.log(err);
+    }
 };
